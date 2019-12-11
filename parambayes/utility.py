1341eea28bf40e40b4ef279eed1d7d3a912dceb4
tlfobe <lennyfobe@gmail.com>
2019-12-10 23:14:58 -0700

    finished pep8 formatting + fixed typing conflict between last merge

--- parambayes/utility.py
+++ parambayes/utility.py
@@ -19,12 +19,22 @@ def rhol_hat_models(compound_2CLJ, Temp, eps, sig, L, Q):
     '''
     if not isinstance(compound_2CLJ, LennardJones_2C):
         raise TypeError(
-            "utility.py:rhol_hat_models: compound_2CLJ must be a LennardJones_2Center_correlation.LennardJones2C object")
-    if all([not isinstance(ins, (float, int, np.float, np.int, np.ndarray)) for ins in [Temp, eps, sig, L, Q]]):
+            "utility.py:rhol_hat_models: " +
+            "compound_2CLJ must be a " +
+            "LennardJones_2Center_correlation.LennardJones2C object")
+    if all([not isinstance(ins,
+                           (float, int, np.float, np.int, np.ndarray))
+            for ins in [Temp, eps, sig, L, Q]]):
         raise TypeError(
-            "utility.py:rhol_hat_models: Temp, eps, sig, L, and Q must be an int/float/np.int/np.float/np.array!")
-    if all([isinstance(ins, (int, float, np.int, np.float)) for ins in [Temp, eps, sig, L, Q]]):
-        raise TypeError("utility.py:rhol_hat_models: At least one of Temp, eps, sig, L or Q must be a Numpy array")
+            "utility.py:rhol_hat_models: " +
+            "Temp, eps, sig, L, and Q must be an " +
+            "int/float/np.int/np.float/np.array!")
+    if all([isinstance(ins,
+                       (int, float, np.int, np.float))
+            for ins in [Temp, eps, sig, L, Q]]):
+        raise TypeError("utility.py:rhol_hat_models: " +
+                        "At least one of Temp, eps, sig, L or Q must be a " +
+                        "Numpy array")
     rhol_hat = compound_2CLJ.rhol_hat_2CLJQ(Temp, eps, sig, L, Q)
     return rhol_hat  # [kg/m3]
 
@@ -35,12 +45,18 @@ def Psat_hat_models(compound_2CLJ, Temp, eps, sig, L, Q):
     """
     if not isinstance(compound_2CLJ, LennardJones_2C):
         raise TypeError(
-            "utility.py:Psat_hat_models: compound_2CLJ must be a LennardJones_2Center_correlation.LennardJones2C object")
-    if all([not isinstance(ins, (float, int, np.float, np.int, np.ndarray)) for ins in [Temp, eps, sig, L, Q]]):
+            "utility.py:Psat_hat_models: compound_2CLJ must be a " +
+            "LennardJones_2Center_correlation.LennardJones2C object")
+    if all([not isinstance(ins, (float, int, np.float, np.int, np.ndarray))
+            for ins in [Temp, eps, sig, L, Q]]):
         raise TypeError(
-            "utility.py:Psat_hat_models: Temp, eps, sig, L, and Q must be an int/float/np.int/np.float/np.array!")
-    if all([isinstance(ins, (int, float, np.int, np.float)) for ins in [Temp, eps, sig, L, Q]]):
-        raise TypeError("utility.py:Psat_hat_models: At least one of Temp, eps, sig, L or Q must be a Numpy array")
+            "utility.py:Psat_hat_models: Temp, eps, sig, L, and Q must be an" +
+            " int/float/np.int/np.float/np.array!")
+    if all([isinstance(ins, (int, float, np.int, np.float))
+            for ins in [Temp, eps, sig, L, Q]]):
+        raise TypeError("utility.py:Psat_hat_models: " +
+                        "At least one of Temp, eps, sig, " +
+                        "L or Q must be a Numpy array")
 
     Psat_hat = compound_2CLJ.Psat_hat_2CLJQ(Temp, eps, sig, L, Q)
 
@@ -55,12 +71,17 @@ def SurfTens_hat_models(compound_2CLJ, Temp, eps, sig, L, Q):
     '''
     if not isinstance(compound_2CLJ, LennardJones_2C):
         raise TypeError(
-            "utility.py:SurfTens_hat_models: compound_2CLJ must be a LennardJones_2Center_correlation.LennardJones2C object")
-    if all([not isinstance(ins, (float, int, np.float, np.int, np.ndarray)) for ins in [Temp, eps, sig, L, Q]]):
+            "utility.py:SurfTens_hat_models: compound_2CLJ must be a " +
+            "LennardJones_2Center_correlation.LennardJones2C object")
+    if all([not isinstance(ins, (float, int, np.float, np.int, np.ndarray))
+            for ins in [Temp, eps, sig, L, Q]]):
         raise TypeError(
-            "utility.py:SurfTens_hat_models: Temp, eps, sig, L, and Q must be an int/float/np.int/np.float/np.array!")
-    if all([isinstance(ins, (int, float, np.int, np.float)) for ins in [Temp, eps, sig, L, Q]]):
-        raise TypeError("utility.py:SurfTens_hat_models: At least one of Temp, eps, sig, L or Q must be a Numpy array")
+            "utility.py:SurfTens_hat_models: Temp, eps, sig, L, and Q must " +
+            "be an int/float/np.int/np.float/np.array!")
+    if all([isinstance(ins, (int, float, np.int, np.float))
+            for ins in [Temp, eps, sig, L, Q]]):
+        raise TypeError("utility.py:SurfTens_hat_models: At least one of " +
+                        "Temp, eps, sig, L or Q must be a Numpy array")
     SurfTens_hat = compound_2CLJ.ST_hat_2CLJQ(Temp, eps, sig, L, Q)
 
     return SurfTens_hat
@@ -74,9 +95,12 @@ def T_c_hat_models(compound_2CLJ, eps, sig, L, Q):
     '''
     if not isinstance(compound_2CLJ, LennardJones_2C):
         raise TypeError(
-            "utility.py:T_c_hat_models: compound_2CLJ must be a LennardJones_2Center_correlation.LennardJones2C object")
-    if any([not isinstance(ins, (int, float, np.int, np.float)) for ins in [eps, sig, L, Q]]):
-        raise TypeError("utility.py:T_c_hat_models: eps, sig, L, and Q must be an int/float/np.int/np.float!")
+            "utility.py:T_c_hat_models: compound_2CLJ must be a " +
+            "LennardJones_2Center_correlation.LennardJones2C object")
+    if any([not isinstance(ins, (int, float, np.int, np.float))
+            for ins in [eps, sig, L, Q]]):
+        raise TypeError("utility.py:T_c_hat_models: eps, sig, L, and Q " +
+                        "must be an int/float/np.int/np.float!")
     T_c_hat = compound_2CLJ.T_c_hat_2CLJQ(eps, sig, L, Q)
     return T_c_hat
 
@@ -100,21 +124,44 @@ def computePercentDeviations(
     """
     if not isinstance(compound_2CLJ, LennardJones_2C):
         raise TypeError(
-            "utility.py:computePercentDeviations: compound_2CLJ must be a LennardJones_2Center_correlation.LennardJones2C object")
-    if any([not isinstance(ins, (int, float, np.int, np.float, np.ndarray)) for ins in [temp_values_rhol, temp_values_psat, temp_values_surftens, rhol_data, psat_data, surftens_data, T_c_data]]):
-        raise TypeError("utility.py:computePercentDeviations: eps, sig, L, and Q must be an int/float/np.int/np.float!")
-    if any([not callable(a) for a in [rhol_hat_models, Psat_hat_models, SurfTens_hat_models, T_c_hat_models]]):
+            "utility.py:computePercentDeviations: compound_2CLJ must be a " +
+            "LennardJones_2Center_correlation.LennardJones2C object")
+    if any([not isinstance(ins, (int, float, np.int, np.float, np.ndarray))
+            for ins in [temp_values_rhol,
+                        temp_values_psat,
+                        temp_values_surftens,
+                        rhol_data, psat_data,
+                        surftens_data,
+                        T_c_data]]):
+        raise TypeError("utility.py:computePercentDeviations: eps, sig, L, " +
+                        "and Q must be an int/float/np.int/np.float!")
+    if any([not callable(a) for a in [rhol_hat_models,
+                                      Psat_hat_models,
+                                      SurfTens_hat_models,
+                                      T_c_hat_models]]):
         raise TypeError(
-            "utility.py:computePercentDeviations: inputs rhol_hat_models, Psat_hat_models, SurfTens_hat_models and T_c_hat_models must be surogate model functions")
-    if not isinstance(parameter_values, list):
-        raise TypeError("utility.py:computePercentDeviations: parameter values must be a list!")
-    rhol_model = rhol_hat_models(compound_2CLJ, temp_values_rhol, *parameter_values)
-    psat_model = Psat_hat_models(compound_2CLJ, temp_values_psat, *parameter_values)
+            "utility.py:computePercentDeviations: " +
+            "inputs rhol_hat_models, Psat_hat_models, " +
+            "SurfTens_hat_models and T_c_hat_models " +
+            "must be surogate model functions")
+    if not isinstance(parameter_values, (list, np.ndarray)):
+        raise TypeError("utility.py:computePercentDeviations: " +
+                        "parameter values must be a list!")
+    rhol_model = rhol_hat_models(compound_2CLJ,
+                                 temp_values_rhol,
+                                 *parameter_values)
+    psat_model = Psat_hat_models(compound_2CLJ,
+                                 temp_values_psat,
+                                 *parameter_values)
     if len(surftens_data) != 0:
-        surftens_model = SurfTens_hat_models(compound_2CLJ, temp_values_surftens, *parameter_values)
-        surftens_deviation_vector = ((surftens_data - surftens_model) / surftens_data)**2
+        surftens_model = SurfTens_hat_models(compound_2CLJ,
+                                             temp_values_surftens,
+                                             *parameter_values)
+        surftens_deviation_vector = \
+            ((surftens_data - surftens_model) / surftens_data)**2
         surftens_mean_relative_deviation = np.sqrt(
-            np.sum(surftens_deviation_vector) / np.size(surftens_deviation_vector)) * 100
+            np.sum(surftens_deviation_vector) /
+            np.size(surftens_deviation_vector)) * 100
     else:
         surftens_mean_relative_deviation = 0
     T_c_model = T_c_hat_models(compound_2CLJ, *parameter_values)
@@ -124,7 +171,14 @@ def computePercentDeviations(
 
     T_c_relative_deviation = (T_c_data - T_c_model) * 100 / T_c_data
 
-    rhol_mean_relative_deviation = np.sqrt(np.sum(rhol_deviation_vector) / np.size(rhol_deviation_vector)) * 100
-    psat_mean_relative_deviation = np.sqrt(np.sum(psat_deviation_vector) / np.size(psat_deviation_vector)) * 100
+    rhol_mean_relative_deviation = \
+        np.sqrt(np.sum(rhol_deviation_vector) /
+                np.size(rhol_deviation_vector)) * 100
+    psat_mean_relative_deviation = \
+        np.sqrt(np.sum(psat_deviation_vector) /
+                np.size(psat_deviation_vector)) * 100
 
-    return rhol_mean_relative_deviation, psat_mean_relative_deviation, surftens_mean_relative_deviation, T_c_relative_deviation
+    return rhol_mean_relative_deviation, \
+        psat_mean_relative_deviation, \
+        surftens_mean_relative_deviation, \
+        T_c_relative_deviation
